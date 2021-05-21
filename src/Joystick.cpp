366a5a24f2aa88164bbe3cfa3a82be762e7b244e
Sebi <sebastian.rast@stud.schulegl.ch>
2021-05-21 21:33:26 +0200

    rewrote mapF functions (without inline)

--- src/Joystick.cpp
+++ src/Joystick.cpp
@@ -1,6 +1,6 @@
 #include "Joystick.h"
 
-inline double mapF(double x, double in_min, double in_max, double out_min, double out_max)
+double mapF(double x, double in_min, double in_max, double out_min, double out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
